9de68f57931f11315fb74c94569cffe848f3e9cc
henriquegrissi <hgcsc2003@gmail.com>
2021-12-08 16:13:40 -0300

    Filtro categorias atualizado com o bd falso

--- Codigo/FiltroCategoria.js
+++ Codigo/FiltroCategoria.js
@@ -1,6 +1,9 @@
 //Esse vetor será integrado com o resto do site, ou seja as notícias não serão as mesmas. Esse é só um exemplo antes de juntarmos as partes
 var idsaude = 0;
 var idpolitica = 1;
+var idconspiracao = 2;
+var idverdadeiro = 3;
+var idfalso = 4;
 var db = {
     categorias: [
         {
@@ -50,19 +53,19 @@ var db = {
             "nomeCategoria": "Conspirações",
             "noticias": [
                 {
-                    "titulo": "",
-                    "link":"",
-                    "descricao": ""
+                    "titulo": "Teoria da conspiração: Lady Di foi assassinada?",
+                    "link":"https://super.abril.com.br/mundo-estranho/teoria-da-conspiracao-lady-di-foi-assassinada/",
+                    "descricao": "A princesa Diana morreu com o namorado em um acidente em 1997. Desde então, há quem acredite que a morte foi encomendada"
                 },
                 {
-                    "titulo": "",
-                    "link":"",
-                    "descricao": ""
+                    "titulo": "Teorias da conspiração: confira as 10 mais bizarras do mundo",
+                    "link":"https://recreio.uol.com.br/ciencia/conheca-10-teorias-da-conspiracao-assustadoras.phtml",
+                    "descricao": "Há teorias da conspiração para todos os gostos: com alienígenas, criaturas do fundo do mar e mais"
                 },
                 {
-                    "titulo": "",
-                    "link":"",
-                    "descricao": ""
+                    "titulo": "Conspirações, aviões de espionagem e OVNIs: a verdade sobre a Área 51",
+                    "link":"http://revistagalileu.globo.com/Revista/Common/0,,EMI341592-18580,00-CONSPIRACOES+AVIOES+DE+ESPIONAGEM+E+OVNIS+A+VERDADE+SOBRE+A+AREA.html",
+                    "descricao": "Depois de anos de segredo, relatório com informações sobre aviões U-2 e a famosa área militar norte-americana é divulgado pela CIA"
                 },
             ]
         },
@@ -71,19 +74,19 @@ var db = {
             "nomeCategoria": "Verdadeiro",
             "noticias": [
                 {
-                    "titulo": "",
-                    "link":"",
-                    "descricao": "Noticias verdadeiras de todas as categorias."
+                    "titulo": "Pfizer e BioNTech afirmam que três doses da vacina neutralizam ômicron",
+                    "link":"https://g1.globo.com/saude/coronavirus/vacinas/noticia/2021/12/08/vacina-da-pfizer-teste-mostra-que-tres-doses-neutralizam-omicron.ghtml",
+                    "descricao": "Fabricantes disseram que três doses aumentam os anticorpos neutralizantes em 25 vezes. Duas doses seguem protegendo contra casos graves da doença, segundo as empresas."
                 },
                 {
-                    "titulo": "",
-                    "link":"",
-                    "descricao": ""
+                    "titulo": "Surto de lesões que provocam coceira tem mariposas como causa, afirma Sociedade Brasileira de Dermatologia em nota técnica",
+                    "link":"https://g1.globo.com/pe/pernambuco/noticia/2021/12/08/sociedade-brasileira-de-dermatologia-emite-nota-tecnica-e-afirma-que-surto-de-lesoes-e-coceira-e-causado-por-mariposas.ghtml  ",
+                    "descricao": "Cerdas do inseto foram encontradas em imagens feitas por meio de microscópio. Esse material, segundo a nota técnica, pode permanecer na pele por dias e até semanas, causando a dermatite."
                 },
                 {
-                    "titulo": "",
-                    "link":"",
-                    "descricao": ""
+                    "titulo": "Capitais mantêm programação de Natal; maioria cancelou réveillon",
+                    "link":"https://g1.globo.com/saude/coronavirus/noticia/2021/12/08/capitais-programacao-natal.ghtml",
+                    "descricao": "Parte delas investiu em decoração e iluminação para o Natal; outras terão apresentações e eventos. Ao menos 22 capitais descartaram fazer festa no Ano Novo em razão da covid."
                 },
             ]
         },
@@ -92,18 +95,18 @@ var db = {
             "nomeCategoria": "Falso",
             "noticias": [
                 {
-                    "titulo": "",
-                    "link":"",
-                    "descricao": "Noticias falsas"
+                    "titulo": "Vacina magnetizada? Microchips na injeção? Veja os fatos sobre vacinas",
+                    "link":"https://agenciabrasil.ebc.com.br/saude/noticia/2021-07/vacina-magnetizada-microchips-na-injecao-veja-os-fatos-sobre-vacinas",
+                    "descricao": "Desinformação compartilhada em redes sociais afeta ritmo da vacinação"
                 },
                 {
-                    "titulo": "",
-                    "link":"",
-                    "descricao": "Noticias falsas"
+                    "titulo": "A “ciência” da Terra plana",
+                    "link":"https://super.abril.com.br/ciencia/a-ciencia-da-terra-plana/",
+                    "descricao": "Se já é difícil entender como tamanha sandice ganhou alguma popularidade, pior ainda é a forma como os terraplanistas distorcem a ciência de verdade para justificar o injustificável."
                 },
                 {
-                    "titulo": "",
-                    "link":"",
+                    "titulo": "Hidroxicloroquina com azitromicina pode ser eficaz no tratamento da Covid-19?",
+                    "link":"https://pebmed.com.br/hidroxicloroquina-com-azitromicina-pode-ser-eficaz-no-tratamento-da-covid-19/",
                     "descricao": "Noticias falsas"
                 },
             ]
