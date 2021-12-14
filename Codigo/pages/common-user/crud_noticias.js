64cad16de8744fe7cafb67da9c554d88a4635a7d
LEON JR FERREIRA <juniohuy2gamer@gmail.com>
2021-12-14 14:43:46 -0300

    Finalização da tela de pesquisa

--- Codigo/pages/common-user/crud_noticias.js
+++ Codigo/pages/common-user/crud_noticias.js
@@ -1,25 +1,63 @@
 var dbn = getNoticiasInLocalStorage();
 var db = getCategoriasInLocalStorage();
+var dbu = getUsersInLocalStorage();
 
 function exibeInformacoes() {
     $("#tbl-noticias").html("");
     for (i = 0; i < dbn.length; i++) {
         let noticia = dbn[i];
+        let user = getUser(noticia.idUsuario);
+        if (user == null) {
+            user = {
+                id: "0",
+                nome: "desconhecido"
+            }
+        }
+        let categoria = getCategoria(noticia.idCategoria);
+        if (categoria == null) {
+            categoria = {
+                id: "0",
+                nome: "desconhecido"
+            }
+        }
         $("#tbl-noticias").append(`<tr>
                                 <td>${noticia.id}</td>
-                                <td>${noticia.usuario}</td>
+                                <td>${user.nome}</td>
                                 <td>${noticia.titulo}</td>
                                 <td>${noticia.descricao}</td>
-                                <td>${noticia.categoria}</td>
-                                <td>${noticia.fonte}</td>
-                                <td>${noticia.data}</td>
+                                <td>${categoria.nome}</td>
+                                <td>${noticia.fonteNoticia}</td>
+                                <td>${noticia.dataPublicada}</td>
                             </tr>`);
     }
+}
+
+function getUser(id) {
+    for (iu = 0; iu < dbu.length; iu++) {
+        if (id == iu) {
+            return dbu[iu];
+        }
+    }
+    return null;
+}
+
+function getCategoria(id2) {
+    for (ic = 0; ic < db.length; ic++) {
+        if (id2 == db[ic].id) {
+            return db[ic];
+        }
+    }
+    return null;
 }
 
 function getNoticiasInLocalStorage() {
     let noticias = JSON.parse(localStorage.getItem("db_noticias"));
     return noticias;
+}
+
+function getUsersInLocalStorage() {
+    let usuarios = JSON.parse(localStorage.getItem("db_usuarios"));
+    return usuarios;
 }
 
 function getCategoriasInLocalStorage() {
